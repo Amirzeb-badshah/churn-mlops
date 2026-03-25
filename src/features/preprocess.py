cd0242c07ac31e78fcdfe1f927cf91bd6962d740
Amirzeb-badshah <mahnorzeb@gmail.com>
2026-03-25 15:10:26 +0500

    Address review: replace dropna with forward fill

--- src/features/preprocess.py
+++ src/features/preprocess.py
@@ -1,5 +1,18 @@
-def clean_data(df):
-    # Remove missing values
-    df = df.drop_duplicates()
-    df = df.fillna(999)
+import pandas as pd
+import logging
+
+def remove_duplicates(df):
+    return df.drop_duplicates()
+
+def handle_missing(df):
+    return df.fillna(method="ffill")
+
+def preprocess_data(df):
+    logging.info("Starting preprocessing")
+
+    df = remove_duplicates(df)
+    df = handle_missing(df)
+
+    logging.info("Finished preprocessing")
+
     return df
